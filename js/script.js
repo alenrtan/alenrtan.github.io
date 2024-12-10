f82de4e712ed4c1619d1d9864e06ecd4c30faaea
Alen Ray <113738058+alenrtan@users.noreply.github.com>
2024-12-09 17:31:44 -0700

    Updates to projects

--- js/script.js
+++ js/script.js
@@ -28,3 +28,53 @@ function burgerMenu() {
     });
 }// end burgerMenu()
 
+// code for auto scrolling
+let autoScrollInterval;
+const scrollContainer = document.querySelector('.data-container-carousel');
+
+function startAutoScroll(){
+    let setTime = 3000;
+    let imageNumber = 1;
+
+    autoScrollInterval = setInterval(() => {
+        imageNumber++
+        if (imageNumber > 4) {
+            imageNumber = 0; // reset 
+        }
+
+
+        const imageID = document.getElementById(`img-${imageNumber}`)
+        if (imageID) {
+            imageID.scrollIntoView({
+                behavior: "smooth",
+                block: "nearest",
+                inline: "center"
+            })
+        }
+    
+
+}, setTime)
+}
+
+
+function stopAutoScroll(){
+    clearInterval(autoScrollInterval);
+    autoScrollInterval = null;
+    console.log("attempting to stop scroll...")
+}
+
+startAutoScroll();
+
+// auto stop after passing
+// this is done since it'll keep pushing the user back to the images 
+window.addEventListener("scroll", () =>{
+    const carousel = scrollContainer.getBoundingClientRect();
+
+    if (carousel.bottom >= 0 && carousel.top <= window.innerHeight && !autoScrollInterval){
+        startAutoScroll();
+    }else if (carousel.bottom < 0 || carousel.top > window.innerHeight){
+        stopAutoScroll();
+    }
+
+    
+})
